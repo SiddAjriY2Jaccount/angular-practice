aa13ad51da1c88536a55c518e296172f08b596e6
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-10-21 00:07:25 +0530

    directives practice

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowServers: boolean = false;
+  allowButtonClick: boolean = false;
+  serverCreateStatus = 'Server was NOT created';
+  serverName: string = '';
+  userName: string = '';
 
   constructor() {
     setTimeout(() => {
       this.allowServers = true;
     }, 5000);
    }
+
+  onCreateServer() {
+    this.serverCreateStatus = `Server was created YESS! Name is ${this.serverName}`;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  setColor() {
+    return this.userName === 'Banana' ? 'lime' : 'red';
+  }
 
   ngOnInit(): void {
   }
