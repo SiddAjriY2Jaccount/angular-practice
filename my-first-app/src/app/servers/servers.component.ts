3bee51b9ac87518c86516f8ff390b9371f3e9be1
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-10-21 00:45:58 +0530

    ngFor done, basics complete

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,11 @@ export class ServersComponent implements OnInit {
   serverCreateStatus = 'Server was NOT created';
   serverName: string = '';
   userName: string = '';
+  servers = [];
+  paraContent: string = 'Displaying details BOOM';
+  paraBool: boolean = false;
+  paraNumArray = [];
+  paraNum = 0;
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreateStatus = `Server was created YESS! Name is ${this.serverName}`;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: Event) {
@@ -29,6 +35,23 @@ export class ServersComponent implements OnInit {
 
   setColor() {
     return this.userName === 'Banana' ? 'lime' : 'red';
+  }
+
+  toggleParaContent() {
+    if (this.paraBool === false) {
+      this.paraContent = 'Adam Cole BABAY!';
+      this.paraBool = true;
+      this.paraNum = this.paraNum + 1;
+      this.paraNumArray.push(this.paraNum);
+    }
+
+    else {
+      this.paraContent = 'KYLE OREILLY Red Dragon is Back!';
+      this.paraBool = false;
+      this.paraNum = this.paraNum + 1;
+      this.paraNumArray.push(this.paraNum);
+    }
+
   }
 
   ngOnInit(): void {
